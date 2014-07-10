722ccfe8ccbff0e8d947079b77c8ace778343aca
Marius Mathiesen <marius@shortcut.no>
2014-07-10 19:38:09 +0200

    Read from STDIN, use sample xml file
    Also generating WTT header with language

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import(
 	"encoding/xml"
 	"fmt"
+	"os"
+	"io/ioutil"
 )
 
 // tt > body > div -> p
 type Feed struct {
 	XMLName xml.Name `xml:"tt"`
 	Screens []*Screen `xml:"body>div>p"`
+	Lang string `xml:"lang,attr"`
 }
 
 type Screen struct {
@@ -19,36 +22,20 @@ type Screen struct {
 
 
 func main(){
-	data := `
-<tt xmlns="http://www.w3.org/ns/ttml" xmlns:tts="http://www.w3.org/2006/04/ttaf1#styling" lang="no">
-  <head>
-    <styling>
-      <style id="italic" tts:fontStyle="italic" />
-      <style id="left" tts:textAlign="left" />
-      <style id="center" tts:textAlign="center" />
-      <style id="right" tts:textAlign="right" />
-    </styling>
-  </head>
-  <body>
-    <div>
-      <p begin="00:00:00.000" dur="00:00:00.000">Copyright (C) NRK</p>
-      <p begin="00:00:08.200" dur="00:00:06.660" style="left">-Jeg er ikke middagen din!<br />-Jeg vet ikke hvem jeg er.</p>
-      <p begin="00:00:15.160" dur="00:00:06.940" style="left">
-	<span style="italic"> -Betjent Jason Stackhouse. </span>
-	<br />-Hvor er du? Du må hjelpe meg.</p>
-      <p begin="00:00:22.400" dur="00:00:04.220" style="left">Takk, åndefar,<br />for at du skjenker oss nytt liv.</p>
-    </div>
-  </body>
-</tt>`
-//	v := Movie{Title: "The great Gatsby"}
+	bytes, ioerr := ioutil.ReadAll(os.Stdin)
+	if ioerr != nil {
+		panic("ZOMG")
+	}
+
 	v := Feed{}
-	err := xml.Unmarshal([]byte(data), &v)
+	err := xml.Unmarshal([]byte(bytes), &v)
 
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 		return
 	}
 
+	fmt.Printf("WEBVTT\n##\nLanguage: %v\n\n", v.Lang)
 	for _, screen := range v.Screens{
 		fmt.Printf("%v ---> %v\n", screen.Begin, screen.Duration)
 		fmt.Printf("%v\n", screen.Text)
