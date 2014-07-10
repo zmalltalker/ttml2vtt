2846807f0a1fdaec809a62b838e0f0124df2872a
Marius Mathiesen <marius@shortcut.no>
2014-07-10 21:46:35 +0200

    Getting the hang of modules. Main is a lot smaller <3

--- parser/parser.go
+++ parser/parser.go
@@ -1,5 +1,8 @@
 package parser
-import("encoding/xml")
+import(
+	"encoding/xml"
+	"fmt"
+)
 
 // tt > body > div -> p
 type Feed struct {
@@ -12,4 +15,18 @@ type Screen struct {
 	Text string `xml:",innerxml"`
 	Begin string `xml:"begin,attr"`
 	Duration string `xml:"dur,attr"`
+}
+
+func ScreensFromXml(bytes []byte) ([]*Screen, string){
+
+	v := Feed{}
+	err := xml.Unmarshal([]byte(bytes), &v)
+
+	if err != nil {
+		panic("XML parsing failed ZOMG")
+	}
+
+	// TODO: Replace with replacer
+
+	return v.Screens, fmt.Sprintf("WEBVTT\n##\nLanguage: %v\n\n", v.Lang)
 }
--- ttml2vtt.go
+++ ttml2vtt.go
@@ -1,7 +1,6 @@
 package main
 
 import(
-	"encoding/xml"
 	"fmt"
 	"os"
 	"io/ioutil"
@@ -14,20 +13,17 @@ import(
 func main(){
 	bytes, ioerr := ioutil.ReadAll(os.Stdin)
 	replacer := strings.NewReplacer("<br />","\n",`<span style="italic">`,"<i>", "</span>","</i>")
+
+
+
 	if ioerr != nil {
 		panic("ZOMG")
 	}
 
-	v := parser.Feed{}
-	err := xml.Unmarshal([]byte(bytes), &v)
+	screens, header := parser.ScreensFromXml(bytes)
 
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return
-	}
-
-	fmt.Printf("WEBVTT\n##\nLanguage: %v\n\n", v.Lang)
-	for _, screen := range v.Screens{
+	fmt.Printf(header)
+	for _, screen := range screens{
 		fmt.Printf("%v ---> %v\n", screen.Begin, screen.Duration)
 		fmt.Printf("%v\n\n", strings.TrimSpace(replacer.Replace(screen.Text)))
 	}
